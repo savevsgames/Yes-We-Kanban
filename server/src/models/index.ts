c74768e995616a5b6e4db3d0deb7b0bfef54a34b
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-10-17 14:40:02 -0600

    env variables and sequelize configuration for render bd named yeswekanbansql

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,24 +1,46 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import { Sequelize } from 'sequelize';
-import { UserFactory } from './user.js';
-import { TicketFactory } from './ticket.js';
+import { Sequelize } from "sequelize";
+import { UserFactory } from "./user.js";
+import { TicketFactory } from "./ticket.js";
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
+const dbName = process.env.DB_NAME || "";
+const dbUser = process.env.DB_USER || "";
+const dbPassword = process.env.DB_PASSWORD || "";
+const dbHost = process.env.DB_HOST || "localhost";
+const dbConnectionString =
+  process.env.DB_CONNECTION_STRING || process.env.DB_URL;
+
+const sequelize = dbConnectionString
+  ? new Sequelize(dbConnectionString, {
+      dialect: "postgres",
       dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // Allows SSL conn.
+        },
         decimalNumbers: true,
       },
+      logging: false,
+    })
+  : new Sequelize(dbName, dbUser, dbPassword, {
+      host: dbHost,
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // Same for parameterized connection
+        },
+        decimalNumbers: true,
+      },
+      logging: false,
     });
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
 
-User.hasMany(Ticket, { foreignKey: 'assignedUserId' });
-Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser'});
+User.hasMany(Ticket, { foreignKey: "assignedUserId" });
+Ticket.belongsTo(User, { foreignKey: "assignedUserId", as: "assignedUser" });
 
 export { sequelize, User, Ticket };
