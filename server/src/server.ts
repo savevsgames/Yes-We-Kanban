478d8f067ba967fb083dcdad554b7c683edc86e3
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-10-17 15:12:56 -0600

    Trying to change port to get render to build - server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,22 @@
 const forceDatabaseRefresh = false;
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import express from 'express';
-import routes from './routes/index.js';
-import { sequelize } from './models/index.js';
+import express from "express";
+import routes from "./routes/index.js";
+import { sequelize } from "./models/index.js";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 
 // Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
+app.use(express.static("../client/dist"));
 
 app.use(express.json());
 app.use(routes);
 
-sequelize.sync({force: forceDatabaseRefresh}).then(() => {
+sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
